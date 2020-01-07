9c8654389d973359cbbb65f6deff2c90a76e8914
wandersonjack <wander8jackson@gmail.com>
2020-01-07 13:40:18 +0200

    applying gesture detector
    the gesture detector notice an event with the onTap and the setState changes the color for example. adding  the verification if the male or female card  is tapped and changing the color.

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -3,7 +3,8 @@ import 'package:bmindex_app/components/item_icon_text.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const activeColor = Color(0xFF262B4D);
+const activeColor = Color(0xFF1d1E33);
+const inactiveColor = Color(0xFF111328);
 const pinkColor = Color(0xFFEB1455);
 const bottomBarHeight = 80.0;
 
@@ -13,6 +14,33 @@ class InputDataPage extends StatefulWidget {
 }
 
 class _InputDataPageState extends State<InputDataPage> {
+  //applying gesture detectors and state for the color change
+  Color maleColor = inactiveColor;//creating initial instance for the colors as inactive
+  Color femaleColor = inactiveColor;//use it inside of the cor of the widget
+
+  //verifying if the tapped widget is male = 1 or female = 2
+  void updateColor(int gender){
+    //male card that was tapped
+    if(gender == 1){
+      //checking if the color is inactive and toggle it with active color
+      if(maleColor == inactiveColor){
+        maleColor = activeColor;
+        femaleColor = inactiveColor;
+      }else{
+        maleColor = inactiveColor;
+      }
+    }else{//it's female
+      //checking the color
+      if(femaleColor == inactiveColor){
+        femaleColor = activeColor;
+        maleColor = inactiveColor;
+      }else{
+        femaleColor = inactiveColor;
+      }
+    }
+
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,20 +53,34 @@ class _InputDataPageState extends State<InputDataPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: CustomCard(
-                    cardChild: ItemIconText(
-                      icone: FontAwesomeIcons.mars,
-                      label: 'MALE',
+                  child: GestureDetector(                                       //wrapping the widget with a gesture detector use onTap and setState to change the color
+                    onTap: (){
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
+                    child: CustomCard(
+                      cor: maleColor,
+                      cardChild: ItemIconText(
+                        icone: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
                     ),
-                    cor: activeColor,
                   ),
                 ),
                 Expanded(
-                  child: CustomCard(
-                    cor: activeColor,
-                    cardChild: ItemIconText(
-                      icone: FontAwesomeIcons.venus,
-                      label: 'FEMALE',
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(2);
+                      });
+                    },
+                    child: CustomCard(
+                      cor: femaleColor,
+                      cardChild: ItemIconText(
+                        icone: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
                     ),
                   ),
                 ),
@@ -75,4 +117,3 @@ class _InputDataPageState extends State<InputDataPage> {
     );
   }
 }
-
